42d85afa5ecdbc4af1fdf8adacdabba6e08bd1cd
Faisal ahmed <faisal2410@yahoo.com>
2023-07-19 12:50:11 +0600

    updated

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,11 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+use ProtoneMedia\Splade\SpladeTable;
 
 class CategoryController extends Controller
 {
     public function index(){
-        $categories=Category::all();
-        return view('categories.index',compact('categories'));
+
+        return view('categories.index',[
+    'categories' => SpladeTable::for(Category::class)
+        ->column('name', canBeHidden: false,sortable: true)
+        ->withGlobalSearch( columns: ['name'] )
+        ->column('slug')
+        ->column('action')
+        ->paginate(5),
+]);
+    }
+
+    public function edit($id){
+
     }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Support\Collection;
+use ProtoneMedia\Splade\SpladeTable;
+use Spatie\QueryBuilder\QueryBuilder;
+use Spatie\QueryBuilder\AllowedFilter;
 
-class PostController extends Controller
-{
-    //
-}
+class PostController extends Controller {
+    public function index() {
+
+        $globalSearch = AllowedFilter::callback( 'global', function ( $query, $value ) {
+            $query->where( function ( $query ) use ( $value ) {
+                Collection::wrap( $value )->each( function ( $value ) use ( $query ) {
+                    $query
+                        ->orWhere( 'title', 'LIKE', "%{$value}%" )
+                        ->orWhere( 'slug', 'LIKE', "%{$value}%" );
+                        // ->orWhere( 'description', 'LIKE', "%{$value}%" );
+                } );
+            } );
+        } );
+
+        $posts = QueryBuilder::for ( Post::class )
+                 ->defaultSort('title')
+                 ->allowedSorts(['title', 'slug', ])
+                 ->allowedFilters(['title', 'slug','category_id', $globalSearch]);
+
+        $categories=Category::pluck('name','id')->toArray();
+
+        return view( 'posts.index', [
+            'posts' => SpladeTable::for ( $posts )
+                    ->column( 'title', canBeHidden: false, sortable: true )
+                    ->withGlobalSearch( columns: ['title'] )
+                    ->column( 'slug',sortable:true )
+                    ->column('action')
+                    ->selectFilter('category_id',$categories)
+                    ->paginate( 5 ),
+            ] );
+        }
+
+        public function edit($id){
+
+        }
+
+    }
+
